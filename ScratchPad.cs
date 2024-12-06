aa96ddc83c93929ceb1707f5012de10577a15deb
Tobias K Persson <tobiape@axis.com>
2024-12-06 18:31:37 +0100

    Initial solution setup

---
+++ ScratchPad.cs
@@ -0,0 +1,11 @@
+﻿// File that may contain copy/pasted code.
+
+//Func<List<string>, List<string>> transpose = (List<string> input) =>
+//	Enumerable
+//		.Range(0, input[0].Length)
+//		.Select(columnIndex => new string(
+//			Enumerable
+//				.Range(0, input.Count)
+//				.Select(rowIndex => input[rowIndex][columnIndex])
+//				.ToArray()))
+//		.ToList();
