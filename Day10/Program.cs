ea8b4d4ea721bed555a1e686fb14b35892619576
Tobias K Persson <tobiape@axis.com>
2024-12-11 08:03:12 +0100

    Solution for day 11

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -7,14 +7,12 @@ string inputFilename = useExampleInput
 List<string> map = File
 	.ReadAllLines(inputFilename)
 	.ToList();
-IList<Position> trailHeads = Enumerable
-	.Range(0, map.Count)
-	.SelectMany(rowIndex => Enumerable
-		.Range(0, map[0].Length)
-		.Where(columnIndex => map[rowIndex][columnIndex] == '0')
-		.Select(columnIndex => new Position(rowIndex, columnIndex)))
-	.ToList();
-
+IList<(int Row, int Column)> trailHeads =
+	(from row in Enumerable.Range(0, map.Count)
+	 from column in Enumerable.Range(0, map[0].Length)
+	 where map[row][column] == '0'
+	 select (row, column))
+		.ToList();
 int resultPartA = trailHeads.Sum(item => ReachableTopPositions(item).Distinct().Count());
 int resultPartB = trailHeads.Sum(item => ReachableTopPositions(item).Count());
 
@@ -22,17 +20,13 @@ Console.WriteLine("Day 10");
 Console.WriteLine($"A: Sum of number of reachable tops is {resultPartA}."); // 36, 822
 Console.WriteLine($"B: Sum of trail ratings is {resultPartB}."); // 81, 1801
 
-IEnumerable<Position> GetUphillSlopeNeighbors(Position position) =>
-	new[] { (Dx: 1, Dy: 0), (Dx: 0, Dy: 1), (Dx: -1, Dy: 0), (Dx: 0, Dy: -1) }
-		.Select(item => new Position(position.Row + item.Dy, position.Column + item.Dx))
-		.Where(item =>
-			item.Row >= 0 && item.Row < map.Count &&
-			item.Column >= 0 && item.Column < map[0].Length)
-		.Where(item => map[item.Row][item.Column] == map[position.Row][position.Column] + 1);
-
-IEnumerable<Position> ReachableTopPositions(Position position) =>
+IEnumerable<(int Row, int Column)> ReachableTopPositions((int Row, int Column) position) =>
 	map[position.Row][position.Column] == '9'
 		? [position]
-		: GetUphillSlopeNeighbors(position).SelectMany(ReachableTopPositions);
-
-record Position(int Row, int Column);
+		: new[] { (Dx: 1, Dy: 0), (Dx: 0, Dy: 1), (Dx: -1, Dy: 0), (Dx: 0, Dy: -1) }
+			.Select(item => (Row: position.Row + item.Dy, Column: position.Column + item.Dx))
+			.Where(item =>
+				item.Row >= 0 && item.Row < map.Count &&
+				item.Column >= 0 && item.Column < map[0].Length)
+			.Where(item => map[item.Row][item.Column] == map[position.Row][position.Column] + 1)
+			.SelectMany(ReachableTopPositions);
---
+++ Day11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+
+bool useExampleInput = false;
+
+string inputFilename = useExampleInput
+	? "exampleInput.txt"
+	: "input.txt";
+
+List<string> input = File
+	.ReadAllLines(inputFilename)[0]
+	.Split(' ')
+	.ToList();
+ConcurrentDictionary<(string Value, int RemainingSteps), long> lookupForValueAndRemainingSteps = new();
+
+long Step(string value, int remainingSteps) =>
+	lookupForValueAndRemainingSteps.GetOrAdd((value, remainingSteps), parameter =>
+		parameter.RemainingSteps == 0 ? 1 :
+		parameter.Value == "0" ? Step("1", parameter.RemainingSteps - 1) :
+		parameter.Value.Length % 2 == 0
+			? Step(parameter.Value[..(parameter.Value.Length / 2)], remainingSteps - 1) +
+				Step(long.Parse(parameter.Value[(parameter.Value.Length / 2)..]).ToString(), remainingSteps - 1) :
+		Step((long.Parse(parameter.Value) * 2024).ToString(), remainingSteps - 1));
+
+long resultPartA = input.Sum(item => Step(item, 25));
+long resultPartB = input.AsParallel().Sum(item => Step(item, 75));
+
+Console.WriteLine("Day 11");
+Console.WriteLine($"A: The number of stones after 25 blinks is {resultPartA}."); // 55312, 194557
+Console.WriteLine($"B: The number of stones after 75 blinks is {resultPartB}."); // 65601038650482, 231532558973909
