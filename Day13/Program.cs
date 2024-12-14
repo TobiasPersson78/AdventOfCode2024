29edcda06329aeaca7364b38691367bd527a43dd
Tobias K Persson <tobiape@axis.com>
2024-12-14 08:41:20 +0100

    Solution for day 14

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -6,31 +6,28 @@ string inputFilename = useExampleInput
 	? "exampleInput.txt"
 	: "input.txt";
 
-List<Machine> clawMachines = File
-	.ReadAllText(inputFilename)
-	.Split("\r\n\r\n")
-	.Select(machine => Regex
-		.Matches(machine, @"\d+")
-		.Select(match => long.Parse(match.Groups[0].Value))
-		.ToList())
-	.Select(numbers => new Machine(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]))
+List<Machine> clawMachines = Regex
+	.Matches(File.ReadAllText(inputFilename), @"\d+")
+	.Select(match => long.Parse(match.Groups[0].Value))
+	.Chunk(6)
+	.Select(item => new Machine(item[0], item[1], item[2], item[3], item[4], item[5]))
 	.ToList();
-long resultPartA = clawMachines.Sum(SolveUsingCramersRule);
+long resultPartA = clawMachines.Sum(SolveLinearEquation);
 long resultPartB = clawMachines.Sum(item =>
-	SolveUsingCramersRule(
+	SolveLinearEquation(
 		item with { TargetX = item.TargetX + 10000000000000L, TargetY = item.TargetY + 10000000000000L }));
 
 Console.WriteLine("Day 13");
 Console.WriteLine($"A: The fewest required tokens is {resultPartA}."); // 480, 27105
 Console.WriteLine($"B: The fewest required tokens is {resultPartB}."); // 875318608908, 101726882250942
 
-long SolveUsingCramersRule(Machine machine)
+long SolveLinearEquation(Machine machine)
 {
 	long determinant = machine.ADeltaX * machine.BDeltaY - machine.ADeltaY * machine.BDeltaX;
 	long numberOfPressesOnA = (machine.TargetX * machine.BDeltaY - machine.TargetY * machine.BDeltaX) / determinant;
 
-	if (numberOfPressesOnA * determinant != (machine.TargetX * machine.BDeltaY - machine.TargetY * machine.BDeltaX))
-		return 0; // No solution
+	if (numberOfPressesOnA * determinant != machine.TargetX * machine.BDeltaY - machine.TargetY * machine.BDeltaX)
+		return 0; // No integer solution. Could also check for >= 0. Could also check that numberOfPressesOnB also is integer >= 0.
 
 	long numberOfPressesOnB = (machine.TargetY - machine.ADeltaY * numberOfPressesOnA) / machine.BDeltaY;
 
---
+++ Day14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+bool useExampleInput = false;
+
+(string inputFilename, int width, int height) = useExampleInput
+	? ("exampleInput.txt", 11, 7)
+	: ("input.txt", 101, 103);
+
+List<(int Row, int Column, int RowSpeed, int ColumnSpeed)> robots = Regex
+	.Matches(File.ReadAllText(inputFilename), @"-?\d+")
+	.Select(match => int.Parse(match.Groups[0].Value))
+	.Chunk(4)
+	.Select(item => (Row: item[1], Column: item[0], RowSpeed: item[3], ColumnSpeed: item[2]))
+	.ToList();
+var moveSteps = (IEnumerable<(int Row, int Column, int RowSpeed, int ColumnSpeed)> itemsToMove, int steps) => itemsToMove
+	.Select(item => (Row: item.Row + steps * item.RowSpeed, Column: item.Column + steps * item.ColumnSpeed))
+	.Select(item => (Row: (item.Row % height + height) % height, Column: (item.Column % width + width) % width));
+var countQuadrants = (IEnumerable<(int Row, int Column)> itemsToCount) => itemsToCount
+	.Aggregate(
+		(TopLeft: 0, TopRight: 0, BottomLeft: 0, BottomRight: 0),
+		(acc, curr) => (
+			TopLeft: acc.TopLeft + (curr.Row < height / 2 && curr.Column < width / 2 ? 1 : 0),
+			TopRight: acc.TopRight + (curr.Row < height / 2 && curr.Column > width / 2 ? 1 : 0),
+			BottomLeft: acc.BottomLeft + (curr.Row > height / 2 && curr.Column < width / 2 ? 1 : 0),
+			BottomRight: acc.BottomRight + (curr.Row > height / 2 && curr.Column > width / 2 ? 1 : 0)));
+var safetyFactor = ((int TopLeft, int TopRight, int BottomLeft, int BottomRight) quadrants) =>
+	quadrants.TopLeft * quadrants.TopRight * quadrants.BottomLeft * quadrants.BottomRight;
+int resultPartA = safetyFactor(countQuadrants(moveSteps(robots, 100)));
+int resultPartB = Enumerable.Range(1, int.MaxValue).First(steps => moveSteps(robots, steps).Distinct().Count() == robots.Count);
+
+Console.WriteLine("Day 14");
+Console.WriteLine($"A: The safety factor is {resultPartA}."); // 12, 217328832
+Console.WriteLine($"B: The number of seconds is {resultPartB}."); // 1 (but really not applicable), 7412
+
+var map = Enumerable.Range(0, height).Select(item => Enumerable.Repeat(' ', width).ToArray()).ToList();
+foreach ((int Row, int Column) position in moveSteps(robots, resultPartB))
+	map[position.Row][position.Column] = 'X';
+Console.ForegroundColor = ConsoleColor.Green;
+foreach (char[] row in map)
+	Console.WriteLine(row);
